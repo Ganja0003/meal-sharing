406df70a706fed5689cab93f666c40fa7cc90171
yuusuf <yuusuf98@outlook.com>
2025-06-04 15:39:51 +0200

    Add ReviewsForm component for submitting reservations

--- app-next/app/meals/[id]/page.jsx
+++ app-next/app/meals/[id]/page.jsx
@@ -1,5 +1,6 @@
 import Meal from "../../../components/MealsList/Meal";
 import ReservationsForm from "@/components/ReservationsForm";
+import ReviewsForm from "@/components/ReviewsForm";
 
 export async function generateStaticParams() {
   const res = await fetch("http://127.0.0.1:3001/api/meals");
@@ -21,7 +22,6 @@ export default async function specificMeal({ params }) {
         <div className="reservation-mealCardContainer">
           <Meal meal={meal} showDetails={true} showReserveButton={false} />
         </div>
-
         <ReservationsForm mealId={params.id} meal={meal} />
       </div>
 
@@ -29,46 +29,7 @@ export default async function specificMeal({ params }) {
       <br />
       <br />
 
-      <form
-        action="http://127.0.0.1:3001/api/reviews"
-        Method="POST"
-        className="reviewForm"
-      >
-        <h1>Reviews</h1>
-
-        <input type="hidden" name="meal_id" value={meal.id} />
-        <input
-          type="hidden"
-          name="created_date"
-          value={new Date().toISOString().split("T")[0]}
-        />
-
-        <div className="reviewFormContainer">
-          <div>
-            <label htmlFor="title">Title:</label>
-            <input type="text" name="title" id="title" required />
-          </div>
-
-          <div>
-            <label htmlFor="review">Description:</label>
-            <textarea
-              id="review"
-              name="description"
-              rows="20"
-              cols="80"
-              placeholder="Write your review here..."
-              required
-            ></textarea>
-          </div>
-
-          <div>
-            <label htmlFor="stars">Stars:</label>
-            <input type="number" max={5} min={1} name="stars" required />
-          </div>
-
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+      <ReviewsForm mealId={params.id} />
     </>
   );
 }
---
+++ app-next/components/ReviewsForm.jsx
@@ -0,0 +1,97 @@
+"use client";
+import { useState } from "react";
+
+export default function ReviewsForm({ mealId }) {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    stars: "",
+  });
+
+  function handleFormInput(event) {
+    const inputName = event.target.name;
+
+    setFormData({
+      ...formData,
+      [inputName]: event.target.value,
+    });
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const dataToSend = [
+      {
+        title: formData.title,
+        description: formData.description,
+        stars: formData.stars,
+        meal_id: parseInt(mealId),
+        created_date: new Date().toISOString().split("T")[0],
+      },
+    ];
+
+    const response = await fetch("http://127.0.0.1:3001/api/reviews", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(dataToSend),
+    });
+
+    try {
+      if (response.ok) {
+        alert("Review Submitted");
+      } else {
+        const err = await response.json();
+        alert(err.message);
+      }
+    } catch (error) {
+      alert("network issue" + err.message);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="reviewForm">
+      <h1>🌟 Reviews</h1>
+
+      <div className="reviewFormContainer">
+        <div>
+          <label htmlFor="title">📝 Title:</label>
+          <input
+            name="title"
+            type="text"
+            value={formData.title}
+            onChange={handleFormInput}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="description">💬 Description:</label>
+          <textarea
+            name="description"
+            value={formData.description}
+            onChange={handleFormInput}
+            rows="20"
+            cols="80"
+            placeholder="Write your review here..."
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="stars">⭐ Stars:</label>
+          <input
+            name="stars"
+            type="number"
+            value={formData.stars}
+            onChange={handleFormInput}
+            min={1}
+            max={5}
+            required
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  );
+}
