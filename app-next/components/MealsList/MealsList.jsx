41647739c55ce2bbff63e5a4c9ce418085169f50
yuusuf <yuusuf98@outlook.com>
2025-09-27 14:45:29 +0200

    Add search bar that filters through meal titles and only display what is inputed to the search field.

--- app-next/components/MealsList/MealsList.jsx
+++ app-next/components/MealsList/MealsList.jsx
@@ -2,10 +2,10 @@
 import { useEffect, useState } from "react";
 import Meal from "./Meal.jsx";
 
-
 // This page can be deleted once you add your own page
-function MealsList({slice}) {
+function MealsList({ slice }) {
   const [meals, setMeals] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     async function fetchMeals() {
@@ -16,20 +16,32 @@ function MealsList({slice}) {
 
     fetchMeals();
   }, []);
-
-  const mealsToRender = slice ? meals.slice(0,3) : meals;
+  console.log(search);
+  const mealsToRender = slice ? meals.slice(0, 3) : meals;
   return (
-    <div className="mealContainer">
-      <div className="mealCardContainer">
-        {mealsToRender.map((meal, index) => {
-          return (
-            <div className="mealCard">
-              <Meal key={index} meal={meal} />
-            </div>
-          );
-        })}
+    <>
+      <div className="search">
+        <label htmlFor="search">Search</label>
+        <input
+          type="text"
+          id="search"
+          onChange={(e) => setSearch(e.target.value)}
+        />
       </div>
-    </div>
+      <div className="mealContainer">
+        <div className="mealCardContainer">
+          {mealsToRender.filter((meal) => {
+            return search === '' ? meal.title : meal.title.toLowerCase().includes(search.toLowerCase())
+          }).map((meal, index) => {
+            return (
+              <div className="mealCard">
+                <Meal key={index} meal={meal} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
