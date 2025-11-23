43423e85ed8f5a8bbe76318dfc9d9452e52b816e
yuusuf <yuusuf98@outlook.com>
2025-11-23 12:05:30 +0100

    Add search input that filters through the meals and displays the searched meal.

--- app-next/components/MealsList/MealsList.jsx
+++ app-next/components/MealsList/MealsList.jsx
@@ -5,7 +5,8 @@ import Meal from "./Meal.jsx";
 // This page can be deleted once you add your own page
 function MealsList({ slice }) {
   const [meals, setMeals] = useState([]);
-  const [search, setSearch] = useState("");
+  const [search,setSearch] = useState('')
+  
 
   useEffect(() => {
     async function fetchMeals() {
@@ -16,23 +17,24 @@ function MealsList({ slice }) {
 
     fetchMeals();
   }, []);
-  console.log(search);
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
+  console.log(search)
+  
   const mealsToRender = slice ? meals.slice(0, 3) : meals;
   return (
     <>
       <div className="search">
         <label htmlFor="search">Search</label>
-        <input
-          type="text"
-          id="search"
-          onChange={(e) => setSearch(e.target.value)}
-        />
+        <input type="text" id='search' onChange={handleSearch}/>
       </div>
       <div className="mealContainer">
         <div className="mealCardContainer">
-          {mealsToRender.filter((meal) => {
-            return search === '' ? meal.title : meal.title.toLowerCase().includes(search.toLowerCase())
-          }).map((meal, index) => {
+          {mealsToRender.filter((meal) => meal.title.toLowerCase().includes(search.toLowerCase()) ? meal : '')
+          .map((meal, index) => {
             return (
               <div className="mealCard">
                 <Meal key={index} meal={meal} />
