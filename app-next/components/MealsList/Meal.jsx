8e131425789db2eea3450758c0e94e06ea097c73
yuusuf <yuusuf98@outlook.com>
2026-01-20 13:54:43 +0100

    Add an image column on the meal table and use that instead of manually adding the images from the frontend.

--- app-next/components/MealsList/Meal.jsx
+++ app-next/components/MealsList/Meal.jsx
@@ -1,69 +1,31 @@
-import samosa from "../../assets/samosa.jpeg";
-import lasagna from "../../assets/lasagna.jpeg";
-import biryani from "../../assets/chicken-biryani.jpeg";
-import pizza from "../../assets/pizza.jpeg";
-import burger from "../../assets/burger.jpeg";
-import falafel from "../../assets/falafel.jpeg";
 import Link from "next/link";
 
-function Meal({ meal, showDetails = false, showReserveButton = true  }) {
-  let mealImage;
-
-  switch (meal.title) {
-    case "Lasagna":
-      mealImage = lasagna;
-      break;
-    case "Chicken biryani":
-      mealImage = biryani;
-      break;
-    case "Samosa":
-      mealImage = samosa;
-      break;
-    case "Falafel Wrap":
-      mealImage = falafel;
-      break;
-      case 'Burger':
-      mealImage = burger;
-      break
-      case 'Pizza':
-      mealImage = pizza;
-      break
-  }
-
-  
+function Meal({ meal, showDetails = false, showReserveButton = true }) {
+ 
   return (
     <>
       <h1 className="mealTitle">{meal.title}</h1>
-      <img src={mealImage?.src} alt="" className="mealImage" />
+      <img src={meal.image_url} alt="" className="mealImage" />
       <p className="mealDescription">{meal.description}</p>
-      
-      
-      {
-        showDetails && (
-          <>
+
+      {showDetails && (
+        <>
           <p>Location: {meal.location}</p>
-          <p>Date: {meal.when.slice(0,10)}</p>
+          <p>Date: {meal.when.slice(0, 10)}</p>
           <p>Available Reservations: {meal.available_reservations}</p>
           <p className="mealPrice">{`Price: ${meal.price} DKK`}</p>
-          
-          </>
-          
-        )
-      }
+        </>
+      )}
 
-        {!showDetails && (
+      {!showDetails && (
         <p className="mealPrice">{`Price: ${meal.price} DKK`}</p>
       )}
-    
 
       {showReserveButton && (
-        <Link href={`/meals/${meal.id}`}><button className="reserveButton">See more</button></Link>
+        <Link href={`/meals/${meal.id}`}>
+          <button className="reserveButton">See more</button>
+        </Link>
       )}
-
-  
-
-     
-
     </>
   );
 }
