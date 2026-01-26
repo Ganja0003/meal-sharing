3eaff1e5dfab0b990f52402d973cd3c03b16c993
yuusuf <yuusuf98@outlook.com>
2026-01-26 14:57:46 +0100

    add fallback to Railway env vars for database connection

--- api/src/database_client.js
+++ api/src/database_client.js
@@ -6,11 +6,11 @@ dotenv.config();
 const db = knex({
   client: process.env.DB_CLIENT,
   connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_NAME,
+    host: process.env.DB_HOST || process.env.MYSQLHOST ,
+    port: process.env.DB_PORT || process.env.MYSQLPORT,
+    user: process.env.DB_USER || process.env.MYSQLUSER,
+    password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
+    database: process.env.DB_DATABASE_NAME || process.env.MYSQLDATABASE,
     ssl:
       process.env.DB_USE_SSL === "true" ? { rejectUnauthorized: false } : false,
   },
