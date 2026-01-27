5163e94a725019e271756ec5c886014ddc3c564d
yuusuf <yuusuf98@outlook.com>
2026-01-27 17:12:17 +0100

    remove local database env fallbacks use railway variables only

--- api/src/database_client.js
+++ api/src/database_client.js
@@ -6,11 +6,11 @@ dotenv.config();
 const db = knex({
   client: "mysql2",
   connection: {
-    host: process.env.DB_HOST || process.env.MYSQLHOST ,
-    port: process.env.DB_PORT || process.env.MYSQLPORT,
-    user: process.env.DB_USER || process.env.MYSQLUSER,
-    password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
-    database: process.env.DB_DATABASE_NAME || process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST ,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
     ssl: { rejectUnauthorized: false },
   },
 });
